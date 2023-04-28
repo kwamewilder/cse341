b81cd944ebd1a7ae2c2b369e303677b5f7b18aaf
kwamewiilder <oheneba.ega@outlook.com>
2023-04-28 21:20:41 +0000

    Lesson 2

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Emmanuel O Gyasi-Ampomah');
-});
+router.use('./contacts', require('./contacts'))
 
 module.exports = router;
