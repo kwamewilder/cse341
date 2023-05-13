63b56e3b9e02481505b9017e2dfeafdae7f8d9e0
kwamewiilder <oheneba.ega@outlook.com>
2023-05-13 16:56:10 +0000

    Week 04

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const swaggerMiddleware = require('./swagger');
 
-router.use('/contacts', require('./contacts'))
+router.use('/contacts', require('./contacts'));
+router.use('/', swaggerMiddleware);
 
 module.exports = router;
